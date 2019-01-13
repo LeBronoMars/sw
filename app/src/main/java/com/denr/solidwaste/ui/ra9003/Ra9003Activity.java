8da2cd2a7f764f0294642f3c0803f2470278510b
lebronomars <lebronomars@gmail.com>
2019-01-13 15:21:21 +0800

    added action bar navigations

--- app/src/main/java/com/denr/solidwaste/ui/ra9003/Ra9003Activity.java
+++ app/src/main/java/com/denr/solidwaste/ui/ra9003/Ra9003Activity.java
@@ -1,9 +1,17 @@
 package com.denr.solidwaste.ui.ra9003;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.denr.solidwaste.BR;
 import com.denr.solidwaste.R;
 import com.denr.solidwaste.base.BaseActivity;
 import com.denr.solidwaste.databinding.ActivityRa9003Binding;
+import com.denr.solidwaste.ui.home.HomeActivity;
+import com.denr.solidwaste.ui.welcome.WelcomeActivity;
 
 import javax.inject.Inject;
 
@@ -12,6 +20,33 @@ public class Ra9003Activity extends BaseActivity<ActivityRa9003Binding, Ra9003Vi
 
     @Inject
     Ra9003ViewModel ra9003ViewModel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.ra_9003_toolbar_title));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.navigation_home:
+                startActivity(new Intent(this, WelcomeActivity.class));
+                finish();
+                return true;
+        }
+        return true;
+    }
 
     @Override
     public int getBindingVariable() {
