39ec620eed36f8729a661df721f5aeb5038ad3ab
lebronomars <lebronomars@gmail.com>
2019-01-13 11:54:14 +0800

    added bottom navigation menu

--- app/src/main/java/com/denr/solidwaste/ui/home/HomeActivity.java
+++ app/src/main/java/com/denr/solidwaste/ui/home/HomeActivity.java
@@ -1,15 +1,33 @@
 package com.denr.solidwaste.ui.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.denr.solidwaste.BR;
 import com.denr.solidwaste.R;
+import com.denr.solidwaste.base.BaseActivity;
+import com.denr.solidwaste.databinding.ActivityHomeBinding;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity<ActivityHomeBinding, HomeViewModel>
+        implements HomeNavigator {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public int getBindingVariable() {
+        return BR.viewModel;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public HomeViewModel getViewModel() {
+        return null;
     }
 }
